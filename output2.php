516d87d41f2942df64d1857b535e972e43ab5e8e
anandaramadana <111747318+anandaramadana@users.noreply.github.com>
2022-08-29 19:39:30 +0700

    Add files via upload

---
+++ output2.php
@@ -0,0 +1,11 @@
+<?php
+    echo ("IDENTITAS DIRI");
+
+    $a = "Ananda Ramadana Ahmad Mulya";
+    $b = 210411100135;
+    $c = 165.5;
+
+    echo "<br> Nama saya ".$a;
+    echo "<br> NIM ".$b;
+    echo "<br> Saya memiliki tinggi badan sebesar ".$c." cm";
+?>
